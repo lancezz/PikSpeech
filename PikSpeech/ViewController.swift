991c12dd102a57763639082a7b7c7cee949795be
lance <cielance0@gmail.com>
2018-10-06 11:45:35 -0700

    Initial Commit

---
+++ PikSpeech/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PikSpeech
+//
+//  Created by Lance Zhang on 2018-10-06.
+//  Copyright © 2018 cmpt275group11. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
